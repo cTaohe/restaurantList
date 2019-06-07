215b3854c8837beb8cd32dc77a7582c0ffd01449
Tao <ctaohe@gmail.com>
2019-06-07 14:28:28 +0800

    S2A1v2

--- app.js
+++ app.js
@@ -18,18 +18,24 @@ app.use(express.static('public'))
 
 // Handle request and response
 app.get('/', (req, res) => {
-    res.render('index', {restaurants: restaurantList.results})
+    res.render('index', { restaurants: restaurantList.results })
 })
 
 app.get('/restaurants/:id', (req, res) => {
-    const restaurant = restaurantList.results.find( restaurant => restaurant.id.toString() === req.params.id)
-    res.render('show', {restaurant: restaurant})
+    const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
+    res.render('show', { restaurant: restaurant })
 })
 
 app.get('/search', (req, res) => {
     const keyword = req.query.keyword
-    const restaurants = restaurantList.results.filter( restaurant => restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) || restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
-    res.render('index', {restaurants: restaurants, keyword: req.query.keyword})
+    const restaurants = restaurantList.results.filter(restaurant => {
+        return (
+            restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) ||
+            restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+            restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+        )
+    })
+    res.render('index', { restaurants: restaurants, keyword })
 })
 
 // Start and listen the server
